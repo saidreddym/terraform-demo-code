5662e60900d4bc360d59157000b48e914cbd8161
saidreddym <saidireddym@gmail.com>
2025-12-29 22:36:30 +0530

    backend

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-# resource "aws_vpc" "abc_test" {
-#   cidr_block =          "10.0.0.0/16"
-# } 
+resource "aws_vpc" "abc_test_dummy_vpc" {
+  cidr_block =          "10.0.0.0/16"
+} 
 
 
 
 
-resource "aws_vpc" "abc_test1" {} 
+# resource "aws_vpc" "abc_test1" {} 
